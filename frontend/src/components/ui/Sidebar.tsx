0fc49f300d04ac32c42977600670e5dd4779e225
Andres Arrieche <andresarrieche11@gmail.com>
2024-03-04 17:39:48 -0400

    corregidas pequeñeces

--- frontend/src/components/ui/Sidebar.tsx
+++ frontend/src/components/ui/Sidebar.tsx
@@ -1,109 +1,89 @@
 import React, { useState } from 'react';
-import Estados from '../../data/Estados.json';
-import estados from '../../assets/estados.png';
-import gestion from '../../assets/gestion.png';
-import Gestion from '../../data/Gestion.json';
+import estados from '../../data/estados.json';
+import estadosPng from '../../assets/estados.png';
+import gestionPng from '../../assets/gestion.png';
+import gestion from '../../data/gestion.json';
 const Sidebar = () => {
-    const [stateSelect, setStateSelect] = useState({
-        Amazonas : false,
-        Anzoategui : false,
-        Apure : false,
-        Aragua : false,
-        Barinas : false,
-        Bolivar : false,
-        Carabobo : false,
-        Cojedes : false,
-        DeltaAmacuro : false,
-        DistritoCapital : false,
-        Falcon : false,
-        Guarico : false,
-        Lara : true,
-        Merida : false,
-        Miranda : false,
-        Monagas : false,
-        NuevaEsparta : false,
-        Portuguesa : false,
-        Sucre : false,
-        Tachira : false,
-        Trujillo : false,
-        Vargas : false,
-        Yaracuy : false,
-        Zulia : false,
+	const [stateSelect, setStateSelect] = useState(
+		Object.fromEntries(estados.map(estado => [estado.nombre, false]))
+	);
 
-    })
-    const [stateFilter, setStateFilter] = useState([]);
+	console.log(stateSelect);
+	const handleOnCheckbox = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setStateSelect({
+			...stateSelect,
+			[e.target.value]: e.target.checked,
+		});
+	};
 
-
-    const [gestionSelect, setGestionSelect] = useState({
-        publico: false,
-        privado: false,
-    })
-    const handleOnCheckbox = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setStateSelect({
-            ...stateSelect,
-            [e.target.value]: e.target.checked
-            }
-        )
-    }
-
-    return (
-    <div>
-        <aside className='sidebar'>
-        <div className='sidebar__hover'>
-                <div className='sidebar__flex'>
-                    <img src={gestion} alt="gestion" />
-                    <h2>Gestion</h2>
-                </div>
-                <ul className='principal'>
-                    <li className='sidebar__list'>
-                        {Gestion.Gestion.tipo.map((gestion) => {
-                            return (
-                                <li className='sidebar__list-li'>
-                                    <input 
-                                        type="radio" 
-                                        name="gestion" 
-                                        id={gestion.id} 
-                                        className='sidebar__li-item' 
-                                        title={gestion.nombre} 
-                                        value={gestion.nombre} />
-                                    <label htmlFor={gestion.id}>{gestion.nombre}</label>
-                                </li>
-                            );
-                        })}
-                    </li>
-                </ul>
-                
-            </div>
-            <div className='sidebar__hover sidebar__hover-vh'>
-                <div className='sidebar__flex'>
-                    <img src={estados} alt="estados" />
-                    <h2>Estados</h2>
-                </div>
-                <ul className='principal'>
-                    <li className='sidebar__list'>
-                    {Estados.Venezuela.estados.map((estado) => {
-                        // Code to render each estado item
-                        return (
-                            <li className="sidebar__list-li">
-                                <input
-                                    onChange={handleOnCheckbox}
-                                    className="sidebar__li-item"
-                                    type="checkbox"
-                                    name={estado.nombre}
-                                    id={estado.id}
-                                    title={estado.nombre}
-                                    value={estado.nombre}
-                                />
-                                <label htmlFor={estado.id}>{estado.nombre}</label>
-                            </li>
-                        );
-                    })}
-                    </li>
-                </ul>
-            </div>
-        </aside>
-    </div>
-    );
+	return (
+		<div>
+			<aside className="sidebar">
+				<div className="sidebar__hover">
+					<div className="sidebar__flex">
+						<img src={gestionPng} alt="gestion" />
+						<h2>Gestion</h2>
+					</div>
+					<ul className="principal">
+						<li className="sidebar__list">
+							{gestion.map((gestion, index) => {
+								return (
+									<div
+										className="sidebar__list-li"
+										key={index}
+									>
+										<input
+											type="radio"
+											name="gestion"
+											id={gestion.id}
+											className="sidebar__li-item"
+											title={gestion.nombre}
+											value={gestion.nombre}
+										/>
+										<label htmlFor={gestion.id}>
+											{gestion.nombre}
+										</label>
+									</div>
+								);
+							})}
+						</li>
+					</ul>
+				</div>
+				<div className="sidebar__hover sidebar__hover-vh">
+					<div className="sidebar__flex">
+						<img src={estadosPng} alt="estados" />
+						<h2>Estados</h2>
+					</div>
+					<ul className="principal">
+						<li className="sidebar__list">
+							{estados.map((estado, index) => {
+								// Code to render each estado item
+								return (
+									<div
+										className="sidebar__list-li"
+										key={index}
+									>
+										<input
+											onChange={handleOnCheckbox}
+											className="sidebar__li-item"
+											type="checkbox"
+											name={estado.nombre}
+											id={estado.id}
+											title={estado.nombre}
+											value={estado.nombre}
+										/>
+										<label htmlFor={estado.id}>
+											{estado.nombre}
+										</label>
+									</div>
+								);
+							})}
+						</li>
+					</ul>
+				</div>
+			</aside>
+		</div>
+	);
 };
 
 export default Sidebar;
